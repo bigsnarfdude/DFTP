a5c25f8e771ceaab782e1ceb650dd1e0028fb414
bigsnarfdude <ohprecio@gmail.com>
2012-11-28 09:31:08 -0500

    conceptual commit

---
+++ client_DFTP.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+'''
+This progam is the client portion of DFTP and it exfiltrates data from a host to a DFTP server that looks and acts like a DNS server. Packets are UDP and look like DNS packets except for payload. This program is built to test DFIR response teams and DFIR tools.
+'''
+
+class Client:
+    def __init__():
+        pass
+
+
---
+++ server_DFTP.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+'''
+This progam is the server portion of DFTP and it is the destination and storage of the exfiltrated data from a host using the DFTP client progam. The server looks and acts like a DNS server. Packets are UDP and look like DNS packets except for payload. This program is built to test DFIR response teams and DFIR tools.
+'''
+
+class Server:
+    def __init__():
+        pass
