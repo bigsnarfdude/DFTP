d5f9971a89557be31544e717e60345bc036ba85d
bigsnarfdude <ohprecio@gmail.com>
2012-11-28 15:21:10 -0500

    basic working idea

--- simple_client.py
+++ simple_client.py
@@ -1,13 +1,19 @@
 import socket
+import time
+
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
- 
+UDP_PORT = 5555
+MESSAGE = "This is the secret message. I love DFTP. This is the end of the secret message."
+SPLIT_MESSAGE = MESSAGE.split()
+
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+for word in SPLIT_MESSAGE:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(word, (UDP_IP, UDP_PORT))
+    time.sleep(2)
+
 
