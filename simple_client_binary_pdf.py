ac82736980571a480a458c3dec8dcec1839596ea
bigsnarfdude <ohprecio@gmail.com>
2012-11-28 21:57:17 -0500

    day one commit

---
+++ simple_client_binary_pdf.py
@@ -0,0 +1,25 @@
+import socket
+import time
+import sys
+import binascii
+import struct
+import string
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 53
+BUFSIZE = 1024
+PDF = "foo.pdf"
+#PDF = "/Users/antigen/dev/DFTP/DNS_Exfiltration.pdf"
+END = "EOF"
+
+fh = open(PDF)
+
+file_buffer = fh.read(BUFSIZE)
+while file_buffer:
+    print "UDP target IP:", UDP_IP
+    print "UDP target port:", UDP_PORT
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(file_buffer, (UDP_IP, UDP_PORT))
+    file_buffer = fh.read(BUFSIZE)
+sock.sendto(END, (UDP_IP, UDP_PORT))
