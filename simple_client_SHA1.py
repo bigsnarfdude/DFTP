6fe6f66bd06fb18d4d3f498279bf15efc26f7ca0
bigsnarfdude <ohprecio@gmail.com>
2012-11-30 08:00:55 -0500

    bunch of hashers

---
+++ simple_client_SHA1.py
@@ -0,0 +1,21 @@
+import socket
+import time
+import hashlib
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5555
+MESSAGE = "This is the secret message. I love DFTP. This is the end of the secret message."
+SPLIT_MESSAGE = MESSAGE.split()
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+for word in SPLIT_MESSAGE:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    m = hashlib.sha1()
+    m.update(word)
+    sock.sendto(m.hexdigest(), (UDP_IP, UDP_PORT))
+    time.sleep(0.5)
+
+
